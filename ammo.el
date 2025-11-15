91778adfe74abe4684fc435f810be2633b354345
arcensyl <dev@arcensyl.me>
2025-11-15 03:32:56 -0600

    Initial commit

---
+++ ammo.el
@@ -0,0 +1,22 @@
+;;; ammo.el - Automatic Manager for Modes -*- lexical-binding: t; -*-
+
+;; Author: Arcensyl <dev@arcensyl.me>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "28.1"))
+;; Keywords: convenience
+
+;;; Commentary:
+
+;;; Code:
+
+;; TODO: Write documentation.
+
+(require 'cl-lib) ;; TODO: Add to dependencies.
+
+(defgroup ammo nil
+  ""
+  :group 'convenience
+  :prefix "ammo-")
+
+(provide 'ammo)
+;;; ammo.el ends here
