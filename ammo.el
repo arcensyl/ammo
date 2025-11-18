1272bd303dd702af73bdd344f861a13b881cfdc2
arcensyl <dev@arcensyl.me>
2025-11-18 04:39:29 -0600

    Added basic trigger system

--- ammo.el
+++ ammo.el
@@ -20,5 +20,43 @@
   :group 'convenience
   :prefix "ammo-")
 
+(defvar ammo--triggers (make-hash-table :test #'eq))
+
+(cl-defstruct ammo--trigger
+  (active-state nil :type boolean)
+  (logic 'and :type symbol)
+  (conditions '() :type list)
+  (activate-fn nil :type function)
+  (deactivate-fn nil :type function))
+
+(cl-defun ammo--check-conditions (trigger)
+  (unless (ammo--trigger-conditions trigger)
+    (cl-return t))
+  
+  (cond ((eq (ammo--trigger-logic trigger) 'and)
+         (cl-loop for condition in (ammo--trigger-conditions trigger)
+                  when (null (ignore-errors (funcall condition)))
+                  return nil
+                  finally return t))
+        ((eq (ammo--trigger-logic trigger) 'or)
+         (cl-loop for condition in (ammo--trigger-conditions trigger)
+                  when (ignore-errors (funcall condition))
+                  return t
+                  finally return nil))
+        (t nil)))
+
+;;;###autoload
+(defun ammo-check-trigger (trigger)
+  (interactive (list (completing-read "Check trigger: "
+                                      (hash-table-keys ammo--triggers)
+                                      nil t)))
+  (when (called-interactively-p)
+    (setq trigger (intern trigger)))
+  
+  (when-let ((actual-trigger (gethash trigger ammo--triggers)))
+    (if (ammo--check-conditions actual-trigger)
+        (message "The conditions of trigger '%s' are met!" trigger)
+      (message "The conditions of trigger '%s' are not met!" trigger))))
+
 (provide 'ammo)
 ;;; ammo.el ends here
